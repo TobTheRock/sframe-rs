93315acbcb5504449421a15d45c1b41721387159
Tobias Waurick <tobtherock@proton.me>
2024-02-17 20:15:06 +0100

    fixup! refactor decrypt

--- src/frame/encrypted_frame.rs
+++ src/frame/encrypted_frame.rs
@@ -31,6 +31,11 @@ impl<'ibuf> EncryptedFrameView<'ibuf> {
         M: AsRef<[u8]> + ?Sized,
     {
         let header = SframeHeader::deserialize(data)?;
+        log::trace!(
+            "EncryptedFrame # {} with header {}",
+            header.frame_count(),
+            header
+        );
 
         Ok(Self {
             header,
@@ -81,6 +86,7 @@ impl<'ibuf> EncryptedFrameView<'ibuf> {
 where {
         let frame_count = self.header().frame_count();
         let key_id = self.header.key_id();
+
         log::trace!(
             "Trying to decrypt EncryptedFrame # {} with KeyId {}",
             frame_count,
@@ -91,6 +97,24 @@ where {
             .get_key(key_id)
             .ok_or(SframeError::MissingDecryptionKey(key_id))?;
 
+        let (aad_buffer, io_buffer) = self.allocate_buffers(buffer)?;
+
+        self.fill_io_buffer(io_buffer);
+        self.fill_aad_buffer(aad_buffer);
+
+        key.decrypt(io_buffer, aad_buffer, frame_count)?;
+
+        let (payload, meta_data) =
+            self.extract_payload_and_metadata(io_buffer, aad_buffer, key.cipher_suite.auth_tag_len);
+
+        let media_frame = MediaFrameView::with_meta_data(frame_count, payload, meta_data);
+        Ok(media_frame)
+    }
+
+    fn allocate_buffers<'a>(
+        &self,
+        buffer: &'a mut impl FrameBuffer,
+    ) -> Result<(&'a mut [u8], &'a mut [u8])> {
         let meta_len = self.meta_data.len();
         let buffer_len_needed = meta_len + self.data.len();
         log::trace!(
@@ -100,22 +124,10 @@ where {
         );
         let frame_buffer = buffer.allocate(buffer_len_needed)?.as_mut();
 
-        let header_len = self.header.len();
-        let aad_buffer_len = header_len + meta_len;
+        let aad_buffer_len = self.header.len() + meta_len;
         let (aad_buffer, io_buffer) = frame_buffer.split_at_mut(aad_buffer_len);
 
-        self.fill_io_buffer(io_buffer);
-        self.fill_aad_buffer(aad_buffer);
-
-        key.decrypt(io_buffer, aad_buffer, frame_count)?;
-
-        // remove tag and header
-        let payload_len = self.data.len() - key.cipher_suite.auth_tag_len - header_len;
-        let payload = &io_buffer[..payload_len];
-        let meta_data = &aad_buffer[..meta_len];
-
-        let media_frame = MediaFrameView::with_meta_data(frame_count, payload, meta_data);
-        Ok(media_frame)
+        Ok((aad_buffer, io_buffer))
     }
 
     fn fill_io_buffer(&self, io_buffer: &mut [u8]) {
@@ -130,6 +142,19 @@ where {
 
         aad_buffer[..meta_len].copy_from_slice(self.meta_data);
         aad_buffer[meta_len..].copy_from_slice(&self.data[..header_len]);
+    }
+
+    fn extract_payload_and_metadata<'obuf>(
+        &self,
+        io_buffer: &'obuf [u8],
+        aad_buffer: &'obuf [u8],
+        auth_tag_len: usize,
+    ) -> (&'obuf [u8], &'obuf [u8]) {
+        let payload_len = self.data.len() - auth_tag_len - self.header.len();
+        let payload = &io_buffer[..payload_len];
+        let meta_data = &aad_buffer[..self.meta_data.len()];
+
+        (payload, meta_data)
     }
 }
 
