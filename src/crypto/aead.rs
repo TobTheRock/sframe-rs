a59ff7524383fb3430674f3661262c87cda9c275
Tobias Waurick <tobtherock@proton.me>
2026-03-03 21:31:52 +0100

    fix: update rand 0.10 API usage (Rng -> RngExt)

--- src/crypto/aead.rs
+++ src/crypto/aead.rs
@@ -34,7 +34,7 @@ mod test {
         util::test::assert_bytes_eq,
     };
 
-    use rand::{Rng, rng};
+    use rand::{RngExt, rng};
     use test_case::test_case;
 
     const KEY_MATERIAL: &str = "THIS_IS_RANDOM";
